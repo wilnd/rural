5b6bd8d49082fa3e03a2aeda8ccd2d02b365185f
xhf319 <370693739@qq.com>
2018-12-08 15:34:38 +0800

    增加服务 rbac-center

---
+++ rural-rbac-center/src/main/java/com/github/rural/rural/rbaccenter/RbacCenterApplication.java
@@ -0,0 +1,14 @@
+package com.github.rural.rural.rbaccenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RbacCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbacCenterApplication.class, args);
+    }
+}
